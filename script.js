dd9bd8929bbe1aed6560347ce65ca715ec3523b6
igotyb <igotyb.stream@gmail.com>
2022-11-07 08:06:05 +0100

    creation of div's

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ console.log("konékté")
 const submitBtn = document.querySelector('.submitBtn');
 // console.log(submitBtn);
 const cityFromUser = document.getElementById('cityFromUser');
-// console.log(cityFromUser);
+console.log(cityFromUser);
+const weatherInDOM = document.querySelector('.weather')
 
 // Change name to coordinates
 async function coordinateWeather(cityChoosen) {
@@ -29,24 +30,60 @@ async function cityWeather(lat, long) {
     }
 
     const getTheWeather = await response.json();
-    console.log(getTheWeather)
+    return getTheWeather
 }
 
 submitBtn.addEventListener('click', () => {
-    cityChoosen = cityFromUser.value
-    console.log(cityChoosen)
+    cityChoosen = cityFromUser.value;
+    console.log(cityChoosen);
 
     coordinateWeather(cityChoosen).then(coordinatesOFCity => {
 
         // console.log(coordinatesOFCity)
-        const lat = coordinatesOFCity[0]
-        // console.log(lat)
-        const long = coordinatesOFCity[1]
-        // console.log(long)
+        const lat = coordinatesOFCity[0];
+        // console.log(lat);
+        const long = coordinatesOFCity[1];
+        // console.log(long);
 
-        cityWeather(lat, long)
+        cityWeather(lat, long).then(cityWeather => {
+            console.log(cityWeather);
+        })
     })
-
-    console.log("Bonjour je suis fait direck'")
+    addToDOM()
 })
+
+const addToDOM = () => {
+    const divWeatherCard = document.createElement('div');
+
+    const divCityImg = document.createElement('div');
+    const cityName = document.createElement('h2');
+
+    const divcityData = document.createElement('div');
+
+    const descriptionDiv = document.createElement('div');
+    const weatherDescription = document.createElement('h3');
+    const descriptionInfos = document.createElement('p');
+
+    const tempDiv = document.createElement('div');
+    const weatherTemp = document.createElement('h3');
+    const tempInfos = document.createElement('p');
+
+    const humidityDiv = document.createElement('div');
+    const weatherHumidity = document.createElement('h3');
+    const humidityInfos = document.createElement('p');
+
+    cityName.innerText = cityFromUser.value
+    weatherDescription.innerText = 'Weather';
+    weatherTemp.innerText = 'Temperature';
+    weatherHumidity.innerText = 'Humidity';
+
+    weatherInDOM.append(divWeatherCard);
+    divWeatherCard.append(divCityImg, divcityData);
+    divCityImg.append(cityName);
+    divcityData.append(descriptionDiv, tempDiv, humidityDiv);
+    descriptionDiv.append(weatherDescription, descriptionInfos);
+    tempDiv.append(weatherTemp, tempInfos);
+    humidityDiv.append(weatherHumidity, humidityInfos)
+}
+
 
